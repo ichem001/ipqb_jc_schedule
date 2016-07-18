5920ed04377f2daa194c97c76cd7a7d9bafe1bf7
Ilan E. Chemmama <ilan.chemmama@ucsf.edu>
2016-07-18 11:06:38 -0700

    Cleaned up the code and made it program length input independent

--- Create_Random_Schedule_Programs.py
+++ Create_Random_Schedule_Programs.py
@@ -1,51 +1,73 @@
 import sys, os
 from random import randint, shuffle
+import argparse
+STDERR = sys.stderr
+
+parser = argparse.ArgumentParser(description="Create a random list of pairs of students with the conditions that two students of the same year should not pair.")
+parser.add_argument('listname', nargs="+", help="List of students")
+
+args = parser.parse_args()
+
+##MN will be the program with the least number of students
+##MJ will be the program with the greater number of students
+MN = []
+MJ = []
 
 BPS = []
 BMI = []
+
 with open(sys.argv[1], 'r') as studentlist:
     for line in studentlist:
         student=line.strip("\n").split(",")
-        if(student[4] == "Biophysics"):
+        if(student[3] == "BP" or student[3] == "Biophysics"):
             BPS.append(student[0:4])
-        elif(student[4] == "Bioinformatics"):
+        elif(student[3] == "BMI" or student[3] == "Bioinformatics"):
             BMI.append(student[0:4])
 
-shuffle(BPS)
-shuffle(BMI)
+###Check which program has the greater number of students
+if len(BPS) > len(BMI):
+    MJ = BPS
+    MN = BMI
+else:
+    MJ=BMI
+    MN=BMI
 
-ub1 = len(BPS)
-ub2 = len(BMI)
 
-while ub1 != 0:
+shuffle(MJ)
+shuffle(MN)
 
-    while ub2 != 0:
-        rnd1 = randint(0, ub1 - 1)
-        rnd2 = randint(0, ub2 - 1)
+mjn = len(MJ)
+mnn = len(MN)
+
+while mjn != 0:
+
+    while mnn != 0:
+        rnd1 = randint(0, mjn - 1)
+        rnd2 = randint(0, mnn - 1)
         ntr = 0
-        while BPS[rnd1][2] == BMI[rnd2][2] and ntr < 10:
-            rnd2 = randint(0, ub2 -1)
+        while MJ[rnd1][2] == MN[rnd2][2] and ntr < 10:
+            rnd2 = randint(0, mnn -1)
             ntr = ntr + 1
-        print(BPS[rnd1], "     ", BMI[rnd2])
-        del BPS[rnd1]
-        del BMI[rnd2]
-        ub1 = len(BPS)
-        ub2 = len(BMI)
+        print "{0:33} {1}".format(",".join(MJ[rnd1]), ",".join(MN[rnd2]))
+        del MJ[rnd1]
+        del MN[rnd2]
+        mjn = len(MJ)
+        mnn = len(MN)
 
     else:
-        if ub1 > 1:
-            rnd1 = randint(0, ub1 - 1)
-            tmp = BPS[rnd1]
-            del BPS[rnd1]
-            ub1 = len(BPS)
-            rnd2 = randint(0, ub1 - 1)
-            print(tmp, "     ", BPS[rnd2])
-            del BPS[rnd2]
-            ub1 = len(BPS)
+        if mjn > 1:
+            rnd1 = randint(0, mjn - 1)
+            tmp = ",".join(MJ[rnd1])
+            del MJ[rnd1]
+            mjn = len(MJ)
+            rnd2 = randint(0, mjn - 1)
+            print "{0:33} {1}".format(tmp,",".join(MJ[rnd2]))
+            del MJ[rnd2]
+            mjn = len(MJ)
 
         else:
-            print(BPS[0])
-            ub1 = 0
+            print ",".join(MJ[0])
+            mjn = 0
 
 
 
